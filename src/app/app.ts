3058a7c5cb35ee49ea84bcdf6410c78aa5de512a
Mo0olTo <mostafa.allam.de@gmail.com>
2026-04-06 01:39:13 +0200

    Dark mood

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Darkmode } from './core/services/darkmode/darkmode';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('FitnessApp');
+  private readonly darkmode=inject(Darkmode)
+   
+  theme= this.darkmode.currentTheme
+
+  toggle() {
+    this.darkmode.toggleTheme();
+  }
 }
