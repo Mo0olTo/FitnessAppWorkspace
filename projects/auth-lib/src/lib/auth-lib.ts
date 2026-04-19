efc8b7fcc61e8f1e3eb6a62c26f315c0ce85e6cb
Rana-hanii <ranahani797@gmail.com>
2026-04-19 04:24:13 +0200

    fix:(update the auth lib with the comments)

--- projects/auth-lib/src/lib/auth-lib.ts
+++ projects/auth-lib/src/lib/auth-lib.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { AuthAPIResService } from './adapter/authAPIRes.adapter';
@@ -25,6 +25,9 @@ import { IUploadPhotoRes } from './interfaces/upload-photo/IPhotoRes';
 import { IVerifyReq } from './interfaces/verify-code/IVerifyReq';
 import { IVerifyRes } from './interfaces/verify-code/IVerifyRes';
 import { API_URL } from './tokens/tokens';
+import { createUrl } from './utilites/helper';
+
+
 
 @Injectable({
   providedIn: 'root',
@@ -33,10 +36,11 @@ export class AuthLib extends authAPI {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = inject(API_URL);
   private readonly authAPIResService = inject(AuthAPIResService);
+  private readonly createUrl = inject(createUrl);
 
   override SignUp(data: ISignUpReq): Observable<AdaptedSignUpRes> {
     return this.http
-      .post<ISignUpRes>(this.createUrl(AuthEndPoint.SIGNUP), data)
+      .post<ISignUpRes>(this.createUrl(this.apiUrl,AuthEndPoint.SIGNUP), data)
       .pipe(
         map((res: ISignUpRes) => this.authAPIResService.adaptSignUp(res)),
         catchError((err) => throwError(() => err))
@@ -45,7 +49,7 @@ export class AuthLib extends authAPI {
 
   override SignIn(data: ISignInReq): Observable<AdaptedSignInRes> {
     return this.http
-      .post<ISignInRes>(this.createUrl(AuthEndPoint.SIGNIN), data)
+      .post<ISignInRes>(this.createUrl(this.apiUrl,AuthEndPoint.SIGNIN), data)
       .pipe(
         map((res: ISignInRes) => this.authAPIResService.adaptSignIn(res)),
         catchError((err) => throwError(() => err))
@@ -54,72 +58,52 @@ export class AuthLib extends authAPI {
 
   override LogOut(): Observable<ILogOutRes> {
     return this.http
-      .get<ILogOutRes>(this.createUrl(AuthEndPoint.LOGOUT))
+      .get<ILogOutRes>(this.createUrl(this.apiUrl,AuthEndPoint.LOGOUT))
       .pipe(catchError((err) => throwError(() => err)));
   }
 
   override ForgetPassword(data: IForgetPasswordReq): Observable<IForgetPasswordRes> {
     return this.http
-      .post<IForgetPasswordRes>(this.createUrl(AuthEndPoint.FORGETPASSWORD), data)
+      .post<IForgetPasswordRes>(this.createUrl(this.apiUrl,AuthEndPoint.FORGETPASSWORD), data)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
   override VerifyCode(data: IVerifyReq): Observable<IVerifyRes> {
     return this.http
-      .post<IVerifyRes>(this.createUrl(AuthEndPoint.VERIFY), data)
+      .post<IVerifyRes>(this.createUrl(this.apiUrl,AuthEndPoint.VERIFY), data)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
   override ResetPassword(data: IResetReq): Observable<IResetRes> {
     return this.http
-      .put<IResetRes>(this.createUrl(AuthEndPoint.RESETPASSWORD), data)
+      .put<IResetRes>(this.createUrl(this.apiUrl,AuthEndPoint.RESETPASSWORD), data)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
-  override ChangePassword(data: IChangePassReq, token: string): Observable<IChangePassRes> {
+  override ChangePassword(data: IChangePassReq): Observable<IChangePassRes> {
     return this.http
-      .patch<IChangePassRes>(this.createUrl(AuthEndPoint.CHANGEPASSWORD), data, {
-        headers: this.buildAuthHeaders(token),
-      })
+      .patch<IChangePassRes>(this.createUrl(this.apiUrl,AuthEndPoint.CHANGEPASSWORD), data)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
-  override EditProfile(data: IEditReq, token: string): Observable<IEditRes> {
+  override EditProfile(data: IEditReq): Observable<IEditRes> {
     return this.http
-      .put<IEditRes>(this.createUrl(AuthEndPoint.EDITPROFILE), data, {
-        headers: this.buildAuthHeaders(token),
-      })
+      .put<IEditRes>(this.createUrl(this.apiUrl,AuthEndPoint.EDITPROFILE), data)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
-  override UploadPhoto(data: IUploadPhotoReq, token: string): Observable<IUploadPhotoRes> {
+  override UploadPhoto(data: IUploadPhotoReq): Observable<IUploadPhotoRes> {
     const formData = new FormData();
     formData.append('photo', data.photo);
 
     return this.http
-      .put<IUploadPhotoRes>(this.createUrl(AuthEndPoint.UPLOADPHOTO), formData, {
-        headers: this.buildAuthHeaders(token),
-      })
+      .put<IUploadPhotoRes>(this.createUrl(this.apiUrl,AuthEndPoint.UPLOADPHOTO), formData)
       .pipe(catchError((err) => throwError(() => err)));
   }
 
-  override GetLoggedUserData(token: string): Observable<IUser> {
+  override GetLoggedUserData(): Observable<IUser> {
     return this.http
-      .get<IUser>(this.createUrl(AuthEndPoint.GETLOGGEDUSERDATA), {
-        headers: this.buildAuthHeaders(token),
-      })
+      .get<IUser>(this.createUrl(this.apiUrl,AuthEndPoint.GETLOGGEDUSERDATA))
       .pipe(catchError((err) => throwError(() => err)));
-  }
-
-
-  // * PRIVATE METHODS TOKEN AND URL
-  private buildAuthHeaders(token: string): HttpHeaders {
-    return new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-  }
-
-  private createUrl(endpoint: string): string {
-    return `${this.apiUrl.replace(/\/+$/, '')}${endpoint}`;
   }
 }
--- projects/auth-lib/src/lib/base/authAPI.ts
+++ projects/auth-lib/src/lib/base/authAPI.ts
@@ -27,12 +27,12 @@ export abstract class authAPI {
   abstract VerifyCode(data: IVerifyReq): Observable<IVerifyRes>;
   // PUT /auth/resetPassword (otp flow)
   abstract ResetPassword(data: IResetReq): Observable<IResetRes>;
-  // PATCH /auth/change-password (requires Authorization: Bearer <token>)
-  abstract ChangePassword(data: IChangePassReq, token: string): Observable<IChangePassRes>;
-  // PUT /auth/edit-profile (requires Authorization: Bearer <token>)
-  abstract EditProfile(data: IEditReq, token: string): Observable<IEditRes>;
-  // PUT /auth/upload-photo (requires Authorization: Bearer <token>)
-  abstract UploadPhoto(data: IUploadPhotoReq, token: string): Observable<IUploadPhotoRes>;
-  // GET logged user data (requires Authorization: Bearer <token>)
-  abstract GetLoggedUserData(token: string): Observable<IUser>;
+  // PATCH /auth/change-password (Authorization via HTTP interceptor)
+  abstract ChangePassword(data: IChangePassReq): Observable<IChangePassRes>;
+  // PUT /auth/edit-profile (Authorization via HTTP interceptor)
+  abstract EditProfile(data: IEditReq): Observable<IEditRes>;
+  // PUT /auth/upload-photo (Authorization via HTTP interceptor)
+  abstract UploadPhoto(data: IUploadPhotoReq): Observable<IUploadPhotoRes>;
+  // GET logged user data (Authorization via HTTP interceptor)
+  abstract GetLoggedUserData(): Observable<IUser>;
 }
