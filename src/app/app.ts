cbec8b8249f0ce01e83c7590cf21052f76f96cb3
Mo0olTo <mostafa.allam.de@gmail.com>
2026-04-19 05:08:08 +0200

    after merge

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from "@angular/router";
 import { FormTitle } from "./features/auth/components/form-title/form-title";
 import { Knob } from "./features/auth/components/knob/knob";
 import { FormButton } from "./features/auth/components/form-button/form-button";
-import { SectionTitle } from "./shared/components/section-title/section-title";
+
 import { ThemeFacade } from './core/Theme/theme.facade';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormTitle, Knob, FormButton, SectionTitle],
+  imports: [RouterOutlet, FormTitle, Knob, FormButton,],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
