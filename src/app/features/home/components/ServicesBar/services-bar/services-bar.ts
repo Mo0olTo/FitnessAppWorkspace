6178f23484732fdea78ad360e8a6e7d60bac7824
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2026-04-19 19:22:01 +0200

    serviceBar

---
+++ src/app/features/home/components/ServicesBar/services-bar/services-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-bar',
+  imports: [],
+  templateUrl: './services-bar.html',
+  styleUrl: './services-bar.scss',
+})
+export class ServicesBar {}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/Hero/hero/hero';
+import { ServicesBar } from './components/ServicesBar/services-bar/services-bar';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, ServicesBar],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
