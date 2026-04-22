64b60cf1624b4d6dd3309af1540ed3d7dafb4dff
mo0olto <mostafa.allam.de@gmail.com>
2026-04-23 01:08:45 +0200

    Auth-forms(ForgetPass-NewPass-OTP)

--- src/app/features/auth/components/auth-form/auth-form.ts
+++ src/app/features/auth/components/auth-form/auth-form.ts
@@ -1,17 +1,20 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { ReusableInput } from "../reusable-input/reusable-input";
 import { FormButton } from "../form-button/form-button";
 import { RouterLink } from "@angular/router";
 import { FormTitle } from "../form-title/form-title";
+import { InputOtpModule } from 'primeng/inputotp';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
-  imports: [ReusableInput, FormButton, RouterLink, FormTitle],
+  imports: [ReusableInput, FormButton, RouterLink, FormTitle ,InputOtpModule ,FormsModule],
   templateUrl: './auth-form.html',
   styleUrl: './auth-form.scss',
 })
 export class AuthForm {
 
-  formType=input<'login' |'register'>('login')
+  formType=input<'login' |'register' | 'forgetPass'| 'otp' | 'newPass'>('login')
+  value = ''
 
 }
