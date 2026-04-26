f3ea73064297455ec0761f97585d4927330da800
mo0olto <mostafa.allam.de@gmail.com>
2026-04-26 15:52:59 +0300

    PrimeNg Toaster , Headers Interceptor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
+import { ToastModule } from 'primeng/toast';
 import { Component, inject} from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { ThemeFacade } from './core/Theme/theme.facade';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ToastModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 
---
+++ src/app/core/interceptors/headers/headers-interceptor.ts
@@ -0,0 +1,19 @@
+import { CookieService } from 'ngx-cookie-service';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+export const headersInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const cookie = inject(CookieService);
+  const token = cookie.get('FitnessToken');
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization : `Bearer ${token}`,
+      }
+    });
+  }
+
+  return next(req);
+};
