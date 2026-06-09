8b61df0b56b62744143e8467dfe1f820910c20c0
mo0olto <mostafa.allam.de@gmail.com>
2026-06-09 11:50:46 +0300

    List Card

--- src/app/features/home/components/why-us/why-us.ts
+++ src/app/features/home/components/why-us/why-us.ts
@@ -1,11 +1,12 @@
 import { Component, input, signal } from '@angular/core';
 import { SectionTitle } from '../../../../shared/components/section-title/section-title';
 import { ReuseableCarousel } from "../../../../shared/components/carousel/carousel";
+import { ListCard } from "../../../../shared/components/listCard/list-card/list-card";
 
 
 @Component({
   selector: 'app-why-us',
-  imports: [SectionTitle, ReuseableCarousel],
+  imports: [SectionTitle, ReuseableCarousel, ListCard],
   templateUrl: './why-us.html',
   styleUrl: './why-us.scss',
 })
@@ -19,4 +20,8 @@ export class WhyUs {
 
   imageHeights = signal<number[]>([378, 344, 285, 346]);
 
+  btnCheck():void{
+    window.alert('Hello From Preview ')
+    
+  }
 }
